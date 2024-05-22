f4943665cf3ef7e03bfb3b742a1b3c0fe852c849
dianamutinda <jaydiana25@gmail.com>
2024-05-22 13:02:12 +0300

    creates a function called  next number

--- counter.js
+++ counter.js
@@ -1,15 +1,3 @@
-//Create a function called sum that takes in two numbers as arguments and returns their sum.
-
-let x;
-let y;
-
-function sum(x,y){
-  console.log(x+y);
-
-}
-sum(3,2);
-sum(-4,-8);
-sum(-3,5);
 
 //Write a function called nextNumber that takes in a number as an argument, increments it by 1 to find the next number, and then returns the next number.  
 
--- solution.js
+++ solution.js
@@ -10,3 +10,14 @@ function sum(x,y){
 sum(3,2);
 sum(-4,-8);
 sum(-3,5);
+
+//Write a function called nextNumber that takes in a number as an argument, increments it by 1 to find the next number, and then returns the next number.  
+
+let num;
+
+function nextNumber(num){
+  console.log(num=num+1);
+}
+nextNumber(1);
+nextNumber(0);
+nextNumber(-2);
